5a42fec925d882a84219df390a6fad92263000b3
unknown <MLejbrink@FLEXIL-WKM19.Flexil.local>
2012-01-26 20:54:13 +0100

    url parse

--- web.js
+++ web.js
@@ -1,18 +1,24 @@
 var express = require('express');
+var url  = require('url');
 var path = require('path');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var stateNr;
+var tamas;
+var updates;
 
 /**
  * GET request.
  */
 app.get('/', function(request, response) {
-	if (request.url == '/?a=updateworld') {
-		stateNr++;
-		response.end('' + request.url + ' Statenr: ' + stateNr);
+	var url_parts = url.parse(request.url, true);
+	var query = url_parts.query;
+
+	if (request.url == '/?a=addtama') {
+		response.end(query);
+	} else if (request.url == '/?a=updateworld') {
+		response.end('' + request.url);
 	} else {
 		servePage(request, response);
 	}
@@ -52,7 +58,9 @@ app.listen(port, function() {
 	console.log("Listening on " + port);
 	
 	// Initial state.
-	stateNr = 1;
+	tamas = new Array();
+	updates = new Array();
+	updates.push('The world is empty.');
 	
 	// Update world within interval.
 	setInterval(function(){updateWorld();}, 100);
