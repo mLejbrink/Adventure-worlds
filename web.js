ab0136b58ff0fe9683ae5dfe5925d8c9ae667dcd
unknown <MLejbrink@FLEXIL-WKM19.Flexil.local>
2012-01-26 19:25:02 +0100

    ticking

--- web.js
+++ web.js
@@ -49,7 +49,11 @@ function servePage(request, response) {
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
+	while(true) {
+		console.log("tick");
+		sleep(5000);
+	}
 });
 
 /**
